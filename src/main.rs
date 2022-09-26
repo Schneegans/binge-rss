4e4dec7e3c150986a5b6ee10b1770dd84f297822
Simon Schneegans <code@simonschneegans.de>
2022-09-26 20:49:25 +0200

    :wrench: Add some more feed parsing

--- src/main.rs
+++ src/main.rs
@@ -4,80 +4,180 @@ use adw::prelude::*;
 use gtk::{gdk, gio};
 
 fn main() {
-    // Register and include resources
-    gio::resources_register_include!("BingeRSS.gresource").expect("register resources");
+  // Register and include resources
+  gio::resources_register_include!("BingeRSS.gresource").expect("register resources");
 
-    let application = adw::Application::builder()
-        .application_id("apps.BingeRSS")
+  let application = adw::Application::builder()
+    .application_id("apps.BingeRSS")
+    .build();
+
+  //
+  application.connect_startup(|_| {
+    adw::init();
+    let display = gdk::Display::default().expect("get default gdk::Display");
+    gtk::IconTheme::for_display(&display).add_resource_path("/");
+  });
+
+  let urls = vec![
+    "https://www.spiegel.de/schlagzeilen/tops/index.rss",
+    "http://reddit.com/r/unixporn/new/.rss?sort=new",
+    "https://omgubuntu.co.uk/feed",
+    "https://www.blendernation.com/feed/",
+    "https://nvd.nist.gov/feeds/xml/cve/misc/nvd-rss-analyzed.xml",
+  ];
+
+  application.connect_activate(move |app| {
+    let sources_list = gtk::ListBox::builder()
+      .css_classes(vec![String::from("content")])
+      .build();
+
+    for url in &urls {
+      let content = sources::get_feed(url);
+
+      if content.is_err() {
+        continue;
+      }
+
+      let content = content.unwrap();
+      let title = &content.title.as_ref().unwrap().content;
+      let subtitle = &content.description.as_ref().unwrap().content;
+
+      let row = adw::ActionRow::builder()
+        .activatable(true)
+        .selectable(false)
+        .title(title)
+        .subtitle(subtitle)
+        .build();
+      sources_list.append(&row);
+
+      // let icon_url = {
+      //   if content.logo.is_some() {
+      //     &content.logo.as_ref().unwrap().uri
+      //   } else if content.icon.is_some() {
+      //     &content.icon.as_ref().unwrap().uri
+      //   } else {
+      //     ""
+      //   }
+      // };
+
+      let url = url::Url::parse(&content.links[0].href);
+      let icon_url = url.as_ref().unwrap().scheme().to_string()
+        + &String::from("://")
+        + &url.as_ref().unwrap().host().unwrap().to_string()
+        + &String::from("/favicon.ico");
+
+      if icon_url != "" {
+        let image = sources::get_image(&icon_url);
+
+        if image.is_ok() {
+          row.add_prefix(&image.unwrap());
+        } else {
+          println!(
+            "Failed to download image {:?} (reason: {:?})",
+            &icon_url, &image
+          );
+        }
+      }
+
+      let subpage = gtk::Box::builder()
+        .orientation(gtk::Orientation::Vertical)
         .build();
 
-    //
-    application.connect_startup(|_| {
-        adw::init();
-        let display = gdk::Display::default().expect("get default gdk::Display");
-        gtk::IconTheme::for_display(&display).add_resource_path("/");
-    });
+      let title = gtk::Label::builder()
+        .label(title)
+        .css_classes(vec![String::from("title")])
+        .build();
 
-    application.connect_activate(|app| {
-        let list = gtk::ListBox::builder()
-            .margin_top(32)
-            .margin_end(32)
-            .margin_bottom(32)
-            .margin_start(32)
-            .css_classes(vec![String::from("content")])
-            .build();
+      let header = adw::HeaderBar::builder().title_widget(&title).build();
+      subpage.append(&header);
 
-        //
-        for _i in 0..10 {
-            let row = adw::ActionRow::builder()
-                .activatable(true)
-                .selectable(false)
-                .title("Click me")
-                .build();
-            list.append(&row);
+      let item_list = gtk::ListBox::builder()
+        .css_classes(vec![String::from("content")])
+        .build();
 
-            let subpage = gtk::Label::new(Some("huhuu"));
+      let item_group = adw::PreferencesGroup::builder()
+        .margin_top(32)
+        .margin_end(32)
+        .margin_bottom(32)
+        .margin_start(32)
+        .build();
+      item_group.add(&item_list);
 
-            row.connect_activated(move |row: &adw::ActionRow| {
-                row.root()
-                    .unwrap()
-                    .downcast_ref::<adw::PreferencesWindow>()
-                    .unwrap()
-                    .present_subpage(&subpage);
-            });
-        }
+      let clamp = adw::Clamp::builder().child(&item_group).build();
 
-        let group = adw::PreferencesGroup::new();
-        group.add(&list);
+      let scrolled_window = gtk::ScrolledWindow::builder()
+        .hscrollbar_policy(gtk::PolicyType::Never)
+        .vexpand(true)
+        .child(&clamp)
+        .build();
 
-        let sources_page = adw::PreferencesPage::builder()
-            .title("Sources")
-            .icon_name("apps.BingeRSS.sources-symbolic")
-            .build();
-        sources_page.add(&group);
+      for item in content.entries {
+        let title = if item.title.is_some() {
+          item.title.as_ref().unwrap().content.clone()
+        } else {
+          String::from("Foo")
+        };
 
-        let preferences_page = adw::PreferencesPage::builder()
-            .title("Preferences")
-            .icon_name("apps.BingeRSS.settings-symbolic")
-            .build();
+        let row = adw::ActionRow::builder()
+          .activatable(true)
+          .title(&title)
+          .selectable(false)
+          .build();
+        item_list.append(&row);
 
-        let window = adw::PreferencesWindow::builder()
-            .application(app)
-            .title("BingeRSS")
-            .default_width(350)
-            .can_navigate_back(true)
-            .build();
+        let url = item.links[0].href.clone();
 
-        window.add(&sources_page);
-        window.add(&preferences_page);
-        window.show();
-    });
+        row.connect_activated(move |_| {
+          let result = gio::AppInfo::launch_default_for_uri(&url, gio::AppLaunchContext::NONE);
+          if result.is_err() {
+            println!("Failed to open URL {}", url);
+          }
+        });
+      }
 
-    let content = sources::get_feed("https://www.spiegel.de/schlagzeilen/tops/index.rss");
-    // let content = sources::get_feed("http://reddit.com/r/unixporn/new/.rss?sort=new");
+      subpage.append(&scrolled_window);
 
-    
-    println!("{:?}", content);
+      row.connect_activated(move |row: &adw::ActionRow| {
+        row
+          .root()
+          .unwrap()
+          .downcast_ref::<adw::PreferencesWindow>()
+          .unwrap()
+          .present_subpage(&subpage);
+      });
+    }
 
-    application.run();
+    let group = adw::PreferencesGroup::builder()
+      .margin_top(32)
+      .margin_end(32)
+      .margin_bottom(32)
+      .margin_start(32)
+      .build();
+    group.add(&sources_list);
+
+    let sources_page = adw::PreferencesPage::builder()
+      .title("Sources")
+      .icon_name("apps.BingeRSS.sources-symbolic")
+      .build();
+    sources_page.add(&group);
+
+    let preferences_page = adw::PreferencesPage::builder()
+      .title("Preferences")
+      .icon_name("apps.BingeRSS.settings-symbolic")
+      .build();
+
+    let window = adw::PreferencesWindow::builder()
+      .application(app)
+      .title("BingeRSS")
+      .default_width(350)
+      .can_navigate_back(true)
+      .build();
+
+    window.add(&sources_page);
+    window.add(&preferences_page);
+    window.add_css_class("devel");
+    window.show();
+  });
+
+  application.run();
 }
--- src/sources.rs
+++ src/sources.rs
@@ -1,7 +1,19 @@
+use gtk::{gdk, gio, glib};
+
 use std::error::Error;
 
-pub fn get_feed(url: &str) -> Result<rss::Channel, Box<dyn Error>> {
-    let content = reqwest::blocking::get(url)?.bytes()?;
-    let channel = rss::Channel::read_from(&content[..])?;
-    Ok(channel)
+pub fn get_feed(url: &str) -> Result<feed_rs::model::Feed, Box<dyn Error>> {
+  let content = reqwest::blocking::get(url)?.bytes()?;
+  let feed = feed_rs::parser::parse(&content[..])?;
+  Ok(feed)
+}
+
+pub fn get_image(url: &str) -> Result<gtk::Image, Box<dyn Error>> {
+  let content = reqwest::blocking::get(url)?.bytes()?;
+  let bytes = glib::Bytes::from(&content.to_vec());
+  let stream = gio::MemoryInputStream::from_bytes(&bytes);
+  let pixbuf = gdk::gdk_pixbuf::Pixbuf::from_stream(&stream, gio::Cancellable::NONE)?;
+  let image = gtk::Image::from_pixbuf(Some(&pixbuf));
+
+  Ok(image)
 }
